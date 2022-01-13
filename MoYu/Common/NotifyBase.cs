32dafb11584db33c4242df39cab4948ca960627c
SundayRX <50036794+SundayRX@users.noreply.github.com>
2022-01-13 14:41:31 +0800

    Add language support zh-hant

---
+++ MoYu/Common/NotifyBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MoYu.Common
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void DoNotify([CallerMemberName] string propName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
